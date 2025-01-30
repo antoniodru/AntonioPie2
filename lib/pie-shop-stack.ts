73266136b85ed5987d10d43f28c20d8b15db2c17
EC2 Default User <ec2-user@ip-10-0-1-227.ec2.internal>
2025-01-30 23:38:09 +0000

    primero

--- lib/pie-shop-stack.ts
+++ lib/pie-shop-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as path from 'path';
 import { Construct } from 'constructs';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PieShopStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PieShopQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  const pieShopSiteBucket = new s3.Bucket(this, 'PieShopSiteBucket',{
+    websiteIndexDocument: 'index.html',
+    blockPublicAccess: new s3.BlockPublicAccess({
+      blockPublicAcls: false,
+      blockPublicPolicy: false,
+      ignorePublicAcls: false,
+      restrictPublicBuckets: false,
+    }),
+    publicReadAccess: true,
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+    autoDeleteObjects: true,
+  });
+  
+    pieShopSiteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [`${pieShopSiteBucket.bucketArn}/*`],
+      principals: [new iam.AnyPrincipal()],
+      effect: iam.Effect.ALLOW,
+    }));
+    
+    new s3deploy.BucketDeployment(this, 'DeployPieShopSite',{
+      sources: [s3deploy.Source.asset(path.join(__dirname,'../site'))],
+      destinationBucket: pieShopSiteBucket,
+      
+  });
+  
   }
 }
