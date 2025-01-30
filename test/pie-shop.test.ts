c77111d7f9ed658d5b2bced201e92f9a4a178671
EC2 Default User <ec2-user@ip-10-0-1-227.ec2.internal>
2025-01-30 22:24:31 +0000

    Initial commit

---
+++ test/pie-shop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PieShop from '../lib/pie-shop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pie-shop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PieShop.PieShopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
